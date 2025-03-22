6d872ec08719106de553736a56baec1ab131383e
Edgar Lira <edgar.jj.29@gmail.com>
2025-03-22 17:46:38 -0300

    feat[database]: define DAOS to user table

--- src/domains/shared/database/daos/base.py
+++ src/domains/shared/database/daos/base.py
@@ -1,1 +1,5 @@
+"""Módulo com declarative_base padrão para todos DAOS usarem"""
 
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
--- src/domains/shared/database/daos/user.py
+++ src/domains/shared/database/daos/user.py
@@ -1,1 +1,17 @@
+from datetime import datetime
+from sqlalchemy import CheckConstraint, Column, String, DateTime
+from .base import Base
 
+
+class UserDao(Base):
+    __tablename__ = "user"
+    __table_args__ = (
+        CheckConstraint(
+            "user_type IN ('admin', 'premium', 'common')", name="user_type_check"
+        ),
+    )
+    email = Column(String(255), primary_key=True)
+    name = Column(String(255))
+    user_type = Column(String(10), default="common", nullable=False)
+    secret_hash = Column(String(255))
+    created_at = Column(DateTime, default=datetime.now)
