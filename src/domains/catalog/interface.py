6b5a875185c7094d6b0ba3c8b329191fd45ca935
Edgar Lira <edgar.jj.29@gmail.com>
2025-03-23 03:17:25 -0300

    feat[catalog]: defining endpoints to catalog domain

--- src/domains/catalog/interface.py
+++ src/domains/catalog/interface.py
@@ -1,1 +1,24 @@
+from fastapi import APIRouter
+from src.domains.catalog.application import CatalogApplication
 
+catalog_router = APIRouter(prefix="/catalog", tags=["Catalog"])
+
+
+@catalog_router.get("/movies")
+async def get_movies():
+    return await CatalogApplication().get_movies()
+
+
+@catalog_router.get("/series")
+async def get_series():
+    return await CatalogApplication().get_series()
+
+
+@catalog_router.post("/movies")
+async def post_movies():
+    return await CatalogApplication().post_movies()
+
+
+@catalog_router.post("/series")
+async def post_series():
+    return await CatalogApplication().post_series()
