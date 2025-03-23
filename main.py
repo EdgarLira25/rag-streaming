c340bd225d4730c59aaed0ed07fa68a0d1564cf2
Edgar Lira <edgar.jj.29@gmail.com>
2025-03-23 02:53:42 -0300

    feat: setting up uvicorn to run on dev mode

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-from src import app
+import uvicorn
+from src.app import app
+from src.domains.shared.database.connector import Database
+
+
+if __name__ == "__main__":
+    Database().migrate_all()
+    uvicorn.run(app, host="0.0.0.0", port=8000)
