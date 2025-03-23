99882fff2deba3d88446ef71f5f3070e39bc4cd0
Edgar Lira <edgar.jj.29@gmail.com>
2025-03-23 03:17:25 -0300

    feat[catalog]: defining endpoints to catalog domain

--- src/domains/catalog/application.py
+++ src/domains/catalog/application.py
@@ -1,1 +1,13 @@
+class CatalogApplication:
 
+    async def get_movies(self):
+        raise NotImplementedError()
+
+    async def get_series(self):
+        raise NotImplementedError()
+
+    async def post_movies(self):
+        raise NotImplementedError()
+
+    async def post_series(self):
+        raise NotImplementedError()
