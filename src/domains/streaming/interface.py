2b9f81e391a609b7e1fbab391ee78582fdc9ea15
Edgar Lira <edgar.jj.29@gmail.com>
2025-03-23 03:18:25 -0300

    feat[stream]: define endpoints to stream interface

--- src/domains/streaming/interface.py
+++ src/domains/streaming/interface.py
@@ -1,1 +1,9 @@
+from fastapi import APIRouter, Request
+from src.domains.streaming.application import StreamingApplication
 
+stream_router = APIRouter(prefix="/stream", tags=["Streaming"])
+
+
+@stream_router.get("/watch")
+async def stream(request: Request):
+    return await StreamingApplication().stream(dict(request.headers))
