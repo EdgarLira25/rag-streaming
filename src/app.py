064ce031079672d812b021aa50fdad06e614d531
Edgar Lira <edgar.jj.29@gmail.com>
2025-03-23 21:30:05 -0300

    feat: '/' route redirect to docs

--- src/app.py
+++ src/app.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from src.domains.user.interface import user_router
 from src.domains.auth.interface import auth_router
 
 app = FastAPI()
 app.include_router(user_router)
 app.include_router(auth_router)
+
+
+@app.get("/")
+def root():
+    return RedirectResponse("/docs")
