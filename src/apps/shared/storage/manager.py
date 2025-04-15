2f353387f8f703100baef7c00d54a8c284d559d6
Edgar Lira <edgar.jj.29@gmail.com>
2025-04-15 19:04:49 -0300

    add typing to storage service

--- src/apps/shared/storage/manager.py
+++ src/apps/shared/storage/manager.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Self
 
 from src.settings import STORAGE_LOCATION
 
@@ -12,7 +12,7 @@ class StorageService:
         self.__base_path = STORAGE_LOCATION
         self.__selected_container = container
 
-    def _delete_empty_dirs(self, dir_path: Path):
+    def _delete_empty_dirs(self, dir_path: Path) -> None:
         current_path = dir_path
         while current_path != self._base_path:
             if any(current_path.iterdir()):
@@ -26,39 +26,40 @@ class StorageService:
             raise NoContainerSelected("Nenhum Container Selecionado")
         return Path(self.__base_path + self.__selected_container)
 
-    def create_container(self, container: str):
+    def create_container(self, container: str) -> None:
         Path(self.__base_path + container).mkdir(exist_ok=True)
 
-    def get_container(self, container: str):
+    def get_container(self, container: str) -> Self:
         self.__selected_container = container
         return self
 
-    def create_blob(self, filename: str, file: bytes):
+    def create_blob(self, filename: str, file: bytes) -> str:
         path = self._base_path / filename
         path.parent.mkdir(exist_ok=True)
         with open(path, "wb") as f:
             f.write(file)
         return filename
 
-    def get_blob(self, filename: str):
+    def get_blob(self, filename: str) -> bytes:
         blob = Path(filename)
         if blob.is_file():
-            blob.read_bytes()
+            return blob.read_bytes()
+        return bytes()
 
-    def list_blobs(self, prefix=""):
+    def list_blobs(self, prefix="") -> list[str]:
         return [
             path.as_posix().replace(self._base_path.as_posix(), "", 1)
             for path in (self._base_path / prefix).rglob("*")
             if path.is_file()
         ]
 
-    def delete_blob(self, filename: str):
+    def delete_blob(self, filename: str) -> None:
         blob = self._base_path / filename
         if Path(blob).is_file():
             blob.unlink()
             self._delete_empty_dirs(blob.parent)
 
-    def delete_many_blobs(self, prefix=""):
+    def delete_many_blobs(self, prefix="") -> None:
         for blob in (self._base_path / prefix).rglob("*"):
             if blob.is_file():
                 blob.unlink()
